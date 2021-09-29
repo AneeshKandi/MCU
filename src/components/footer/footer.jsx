af7339d0463e02ca7c840132424af89a97e924d5
AneeshKandi <85028192+AneeshKandi@users.noreply.github.com>
2021-09-29 16:59:35 +0530

    Update footer.jsx

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -12,8 +12,8 @@ function func(){
     <div className="footer-div" id="contact">
     <div style={{paddingTop: 30}}>
     <a href="https://www.instagram.com" className="items"><InstagramIcon /></a>
-    <a href="https://www.instagram.com" className="items"><FacebookIcon /></a>
-    <a href="https://www.instagram.com" className="items"><LinkedinIcon /></a>
+    <a href="https://www.facebook.com" className="items"><FacebookIcon /></a>
+    <a href="https://www.linkedin.com/in/aneesh-kandi-334b66218?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BCD1g7N98TeO8AWqB0dI3wA%3D%3D" className="items"><LinkedinIcon /></a>
     <h5 style={{paddingTop:5}}> CFI Shop | Copyright 2021</h5>
     </div>
     </div>);
